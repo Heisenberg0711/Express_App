f432e48b40ad237f5996a976527887c712008697
Haotian Qiu <heisenberg0711@gmail.com>
2019-09-12 18:28:10 -0700

    corrected some parts

---
+++ Express/src/google_api/Location.java
@@ -0,0 +1,31 @@
+package google_api;
+
+public class Location {
+	private Double latitude;
+	private Double longitude;
+	
+	public Location(Double latitude, Double longitude) {
+		this.latitude =  latitude;
+		this.longitude = longitude;
+	}
+	public Location() {
+		this.latitude =  null;
+		this.longitude = null;
+	}
+	
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+
+}
